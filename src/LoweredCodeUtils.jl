f84d2f42e334c69e59dc12a1c297582f8d983804
Tim Holy <tim.holy@gmail.com>
2019-02-09 06:57:51 -0600

    Files generated by PkgTemplates

---
+++ src/LoweredCodeUtils.jl
@@ -0,0 +1,5 @@
+module LoweredCodeUtils
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using LoweredCodeUtils
+using Test
+
+@testset "LoweredCodeUtils.jl" begin
+    # Write your own tests here.
+end
