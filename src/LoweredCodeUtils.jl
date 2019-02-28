5c55a0e4e8e52da5b5a7f715959d820b862a9070
Tim Holy <tim.holy@gmail.com>
2019-02-28 11:24:17 -0600

    Signatures for generated function having an anonymous function

--- src/LoweredCodeUtils.jl
+++ src/LoweredCodeUtils.jl
@@ -289,7 +289,13 @@ function correct_name!(stack, frame, pc, name, parentname)
         if name != cname
             replacename!(frame.code.code.code, name=>cname)
         end
-        name = pc_expr(frame, pctop-1).args[1]
+        stmt = pc_expr(frame, lastpcparent)
+        while !ismethod(stmt)
+            lastpcparent = next_or_nothing(frame, lastpcparent)
+            lastpcparent === nothing && return name, lastpcparent
+            stmt = pc_expr(frame, lastpcparent)
+        end
+        name = stmt.args[1]
     end
     return name, pc
 end
