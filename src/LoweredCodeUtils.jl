155da9fd9c359333e5d5ea571f7d39c259b795c6
David Anthoff <anthoff@berkeley.edu>
2020-06-20 06:11:47 -0700

    Rearrange files for easier VS Code integration (#36)

--- src/LoweredCodeUtils.jl
+++ src/LoweredCodeUtils.jl
@@ -1,55 +1,19 @@
 module LoweredCodeUtils
 
-if isdefined(Base, :Experimental) && isdefined(Base.Experimental, Symbol("@optlevel"))
-    @eval Base.Experimental.@optlevel 1
-end
+# We use a code structure where all `using` and `import`
+# statements in the package that load anything other than
+# a Julia base or stdlib package are located in this file here.
+# Nothing else should appear in this file here, apart from
+# the `include("packagedef.jl")` statement, which loads what
+# we would normally consider the bulk of the package code.
+# This somewhat unusual structure is in place to support
+# the VS Code extension integration.
 
-using Core: SimpleVector, CodeInfo, NewvarNode, GotoNode
-using Base.Meta: isexpr
 using JuliaInterpreter
 using JuliaInterpreter: SSAValue, SlotNumber, Frame
 using JuliaInterpreter: @lookup, moduleof, pc_expr, step_expr!, is_global_ref, is_quotenode, whichtt,
                         next_until!, finish_and_return!, get_return, nstatements, codelocation, linetable
 
-const SSAValues = Union{Core.Compiler.SSAValue, JuliaInterpreter.SSAValue}
-
-const structheads = VERSION >= v"1.5.0-DEV.702" ? () : (:struct_type, :abstract_type, :primitive_type)
-const trackedheads = (:method, structheads...)
-
-export signature, rename_framemethods!, methoddef!, methoddefs!, bodymethod
-export CodeEdges, lines_required, lines_required!, selective_eval!, selective_eval_fromstart!
-
-include("utils.jl")
-include("signatures.jl")
-include("codeedges.jl")
-
-# precompilation
-
-if ccall(:jl_generating_output, Cint, ()) == 1
-    kwdefine = NamedTuple{(:define,),Tuple{Bool}}
-    for ct in (Vector{Any}, Set{Any})
-        f = methoddef!
-        m = which(f, Tuple{Function, ct, Frame, Expr, Int})
-        @assert precompile(Tuple{typeof(f), Function, ct, Frame, Expr, Int})
-        mbody = bodymethod(m)
-        # @assert precompile(Tuple{mbody.sig.parameters[1], Bool, typeof(f), Function, ct, Frame, Expr, Int})
-        @assert precompile(Tuple{Core.kwftype(typeof(f)), kwdefine, typeof(f), Function, ct, Frame, Expr, Int})
-        f = methoddefs!
-        @assert precompile(Tuple{typeof(f), Any, ct, Frame})
-        @assert precompile(Tuple{Core.kwftype(typeof(f)), kwdefine, typeof(f), Function, ct, Frame})
-    end
-    @assert precompile(Tuple{typeof(rename_framemethods!), Any, Frame, Dict{Symbol,MethodInfo},
-                             Vector{NamedTuple{(:linetop, :linebody, :callee, :caller),Tuple{Int64,Int64,Symbol,Union{Bool, Symbol}}}},
-                             Dict{Symbol,Union{Bool, Symbol}}})
-    @assert precompile(Tuple{typeof(identify_framemethod_calls), Frame})
-    @assert precompile(Tuple{typeof(callchain), Vector{NamedTuple{(:linetop, :linebody, :callee, :caller),Tuple{Int64,Int64,Symbol,Union{Bool, Symbol}}}}})
-
-    @assert precompile(CodeEdges, (CodeInfo,))
-    @assert precompile(add_links!, (Pair{Union{SSAValue,SlotNumber,NamedVar},Links}, Any, CodeLinks))
-    @assert precompile(lines_required!, (Vector{Bool}, Set{NamedVar}, CodeInfo, CodeEdges))
-
-    precompile(Tuple{typeof(setindex!),Dict{Union{GlobalRef, Symbol},Links},Links,Symbol})
-    precompile(Tuple{typeof(setindex!),Dict{Union{GlobalRef, Symbol},Variable},Variable,Symbol})
-end
+include("packagedef.jl")
 
 end # module
