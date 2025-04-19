41bddfb24be89938aa364982fb58ee19bcbb3bde
Tim Holy <tim.holy@gmail.com>
2025-04-19 01:29:34 -0500

    Restore `trackedheads`
    - Fixes https://github.com/timholy/Revise.jl/issues/906
    - removes a compilation warning about conflict in SSAValue and
      SlotNumber.
    - improves handling of `setglobal!`

--- src/LoweredCodeUtils.jl
+++ src/LoweredCodeUtils.jl
@@ -11,7 +11,7 @@ module LoweredCodeUtils
 
 using JuliaInterpreter
 using JuliaInterpreter: SSAValue, SlotNumber, Frame
-using JuliaInterpreter: @lookup, moduleof, pc_expr, step_expr!, is_global_ref, is_quotenode_egal, whichtt,
+using JuliaInterpreter: @lookup, moduleof, pc_expr, step_expr!, is_global_ref, is_global_ref_egal, is_quotenode_egal, whichtt,
                         next_until!, finish_and_return!, get_return, nstatements, codelocation, linetable,
                         is_return, lookup_return
 
